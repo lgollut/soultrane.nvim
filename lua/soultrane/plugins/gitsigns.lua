754ce74e1e4185182aa213b4276b9577cc78b929
Louis Gollut <louis.gollut@liip.ch>
2024-10-25 09:40:31 +0200

    Add line blame to gitsign config

--- lua/soultrane/plugins/gitsigns.lua
+++ lua/soultrane/plugins/gitsigns.lua
@@ -1,6 +1,10 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 250,
+    },
     on_attach = function(bufnr)
       local gitsigns = require 'gitsigns'
 
