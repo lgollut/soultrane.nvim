54f307a77de6a45a7dfab060ad15f131f4354dad
Louis Gollut <louis.gollut@liip.ch>
2024-10-30 16:59:40 +0100

    Add keymap to copy in the OS clipboard

--- lua/soultrane/remap.lua
+++ lua/soultrane/remap.lua
@@ -3,3 +3,7 @@ vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open Explorer' })
 vim.keymap.set('i', 'jk', '<esc>', { desc = 'Exit insert mode' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
